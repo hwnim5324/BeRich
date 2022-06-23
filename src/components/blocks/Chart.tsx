b4d505339e5c9768652e573a717353ffbbf6107b
남현우 <hwnim5324@naver.com>
2022-06-24 02:43:20 +0900

    making candlestick chart using svg.

--- src/components/blocks/Chart.tsx
+++ src/components/blocks/Chart.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import {  } from 'react-chartjs-2';
 
-// import '../../styles/blocks/Chart.scss';
+import '../../styles/blocks/Chart.scss';
 
 interface chartData {
     date : number,
@@ -11,15 +10,44 @@ interface chartData {
     stck_clpr : number
 };
 
-let aapl = new Array<chartData>();
-aapl = [{date : 20220401, stck_hgpr : 80000, stck_lwpr : 62000, stck_oprc : 78000, stck_clpr : 70000},
+let exampleData = new Array<chartData>();
+exampleData = [{date : 20220401, stck_hgpr : 80000, stck_lwpr : 62000, stck_oprc : 78000, stck_clpr : 70000},
     {date : 20220402, stck_hgpr : 82000, stck_lwpr : 66000, stck_oprc : 74000, stck_clpr : 86000}
 ];
 
-const Chart = ( ) : JSX.Element => {
+const Chart = () : JSX.Element => {
+    
+    const YLABELSIZE = 30;  //날짜 표기 높이
+    const XLABELSIZE = 70;  //가격 표기 넓이
+    const RATIO = 9/16;     //가로 : 세로 = 16 : 9
+    
+
+    // let chartWidth = document.getElementById("TradingPage")?.offsetWidth !== undefined ? document.getElementById("TradingPage")?.offsetWidth as number * 8.5 : 1000;
+    // chartWidth = chartWidth as number;
+    let chartWidth = 1000;
+    let chartHeight = chartWidth * RATIO;
+        
+    // 부모 컴포넌트의 가로를 가져와 변화에 따라 동적으로 차트 가로 길이를 설정해주어야함.
+    
+    // if(document.getElementById("TradingPage")?.offsetWidth===undefined){
+    //     chartWidth = 1000;
+    //     chartHeight = chartWidth * RATIO;
+    // }else{
+    //     chartWidth = document.getElementById("TradingPage")?.offsetWidth;
+    //     chartHeight = chartWidth * RATIO;
+    // }
+    
+    
+
     return(
-        <div>하이</div>
+        <div id='CandleStick'>
+            <svg width={chartWidth} height={chartHeight}>
+                <line x1={0} y1={chartHeight-YLABELSIZE} x2={chartWidth-XLABELSIZE} y2={chartHeight-YLABELSIZE} />
+                <line x1={chartWidth-XLABELSIZE} y1={YLABELSIZE} x2={chartWidth-XLABELSIZE} y2={chartHeight-YLABELSIZE} />
+            </svg>
+        </div>
     );
 }
+
 
 export default Chart;
---
+++ src/hooks/DrawCandleStick.ts
@@ -0,0 +1,29 @@
+import React from 'react';
+
+interface chartData {
+    date : number,
+    stck_hgpr : number,
+    stck_lwpr : number,
+    stck_oprc : number,
+    stck_clpr : number
+};
+
+let exampleData = new Array<chartData>();
+exampleData = [{date : 20220401, stck_hgpr : 80000, stck_lwpr : 62000, stck_oprc : 78000, stck_clpr : 70000},
+    {date : 20220402, stck_hgpr : 82000, stck_lwpr : 66000, stck_oprc : 74000, stck_clpr : 86000}
+];
+
+const YLABELSIZE = 30;  //날짜 표기 높이
+const XLABELSIZE = 70;  //가격 표기 넓이
+const RATIO = 9/16;     //가로 : 세로 = 16 : 9
+
+let chartWidth = 1000;
+let chartHeight = chartWidth * RATIO;
+
+const dataYMax = Math.max.apply(Math, exampleData.map(function(d) { return d.stck_hgpr; }))
+const dataYMin = Math.min.apply(Math, exampleData.map(function(d) { return d.stck_lwpr; }))
+
+const dataYRange = dataYMax - dataYMin;
+const yThicks = 7;
+const barPlotWidth = (chartWidth - XLABELSIZE) /exampleData.length;
+
