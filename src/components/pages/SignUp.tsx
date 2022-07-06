d8f88b0e082ee117492392da0eca574d4972b3b9
남현우 <hwnim5324@naver.com>
2022-07-06 14:50:18 +0900

    signup in progress.

--- src/components/pages/SignUp.tsx
+++ src/components/pages/SignUp.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 
+import LoginInput from "../blocks/LoginInput";
+
+import '../../styles/pages/SignUp.scss';
+
 const SignUp = () : JSX.Element => {
     return (
-        <>
-            회원가입
-        </>
+        <section>
+            <div id="signup">
+                <p id='title'>회원가입</p>
+                <LoginInput label="이름" type="text" />
+                <LoginInput label="아이디" type="text" />
+                <LoginInput label="비밀번호" type="password" />
+                <LoginInput label="비밀번호 확인" type="password" />
+                <button>회원가입</button>
+            </div>
+        </section>
     );
 }
 
