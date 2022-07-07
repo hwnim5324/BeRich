ff33a765b58d0921f2bf3eb8a02f1eabdba7d737
남현우 <hwnim5324@naver.com>
2022-07-07 19:53:02 +0900

    login, signup in progress.

--- src/components/pages/LogIn.tsx
+++ src/components/pages/LogIn.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from "react-router-dom";
-
-import LoginInput from '../blocks/LoginInput';
 
 import '../../styles/pages/LogIn.scss';
 
@@ -9,15 +7,29 @@ import '../../styles/pages/LogIn.scss';
 
 
 const LogIn = ( ) : JSX.Element => {
+
+    const [id, setId] = useState('');
+    const [pw, setPw] = useState('');
+
     return(
         <section id='login'>
             <p id='title'>로그인</p>
-            <LoginInput label='아이디' type='text' />
-            <LoginInput label='비밀번호' type='password'/>
-            <button id='login-submit'>로그인</button>
+            <div id='login-input'>
+                <label>아이디</label>
+                <input type='text' onChange={(e)=>{setId(e.target.value);}}/>
+            </div>
+            <div id='login-input'>
+                <label>비밀번호</label>
+                <input type='password' onChange={(e)=>{setPw(e.target.value);}}/>
+            </div>
+            <button id='login-submit' onClick={login}>로그인</button>
             <p id='signup-link'>계정이 없으신가요? <Link to='/signup' id='link'><span>회원가입</span></Link></p>
         </section>
     );
+}
+
+function login(){
+    
 }
 
 export default LogIn;
--- src/components/pages/SignUp.tsx
+++ src/components/pages/SignUp.tsx
@@ -1,22 +1,42 @@
-import React from "react";
-
-import LoginInput from "../blocks/LoginInput";
+import React, {useState} from "react";
 
 import '../../styles/pages/SignUp.scss';
 
 const SignUp = () : JSX.Element => {
+
+    const [name, setName] = useState('');
+    const [id, setId] = useState('');
+    const [pw, setPw] = useState('');
+    const [check, setCheck] = useState('');
+
     return (
         <section>
             <div id="signup">
                 <p id='title'>회원가입</p>
-                <LoginInput label="이름" type="text" />
-                <LoginInput label="아이디" type="text" />
-                <LoginInput label="비밀번호" type="password" />
-                <LoginInput label="비밀번호 확인" type="password" />
-                <button>회원가입</button>
+                <div id='signup-input'>
+                    <label>이름</label>
+                    <input type='text' onChange={(e)=>{setName(e.target.value);}}/>
+                </div>
+                <div id='signup-input'>
+                    <label>아이디</label>
+                    <input type='text' onChange={(e)=>{setId(e.target.value);}}/>
+                </div>
+                <div id='signup-input'>
+                    <label>비밀번호</label>
+                    <input type='password' onChange={(e)=>{setPw(e.target.value);}}/>
+                </div>
+                <div id='signup-input'>
+                    <label>비밀번호 확인</label>
+                    <input type='password' onChange={(e)=>{setCheck(e.target.value);}}/>
+                </div>
+                <button onClick={signup}>회원가입</button>
             </div>
         </section>
     );
+}
+
+function signup(){
+
 }
 
 export default SignUp;
