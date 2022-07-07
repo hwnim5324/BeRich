bffd71af5a757429f6b14cca03594c1bed021683
남현우 <hwnim5324@naver.com>
2022-07-07 21:42:47 +0900

    sell, buy in progress.

--- src/components/blocks/Trade.tsx
+++ src/components/blocks/Trade.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { Link, NavLink } from "react-router-dom";
 
 import SearchBar from './SearchBar';
 
@@ -36,43 +37,66 @@ const Trading = ( props : { children : React.ReactNode } ) : JSX.Element => {
 const TradeNavigator = () : JSX.Element => {
     return (
         <ul>
-            <li>매수</li>
-            <li>매도</li>
-            <li>정정/취소</li>
-            <li>체결/예약</li>
+            <li><NavLink to='/trading/buy' className={({ isActive }) => isActive ? 'link-active' : 'link'}>매수</NavLink></li>
+            <li><NavLink to='/trading/sell' className={({ isActive }) => isActive ? 'link-active' : 'link'}>매도</NavLink></li>
+            <li><NavLink to='/trading/revise' className={({ isActive }) => isActive ? 'link-active' : 'link'}>정정/취소</NavLink></li>
+            <li><NavLink to='/trading/orderlist' className={({ isActive }) => isActive ? 'link-active' : 'link'}>체결/예약</NavLink></li>
         </ul>
     );
 }
 
 const Buy = () : JSX.Element => {
     return (
-        <>
-            buy
-        </>
+        <div id='buy'>
+            <ul>
+                <li><NavLink to=''>지정가</NavLink></li>
+                <li><NavLink to=''>시장가</NavLink></li>
+            </ul>
+            <div><input type='number' />원</div>
+            <div><input type='number' />주</div>
+            <ul><li>현금최대가능</li><li>주</li></ul>
+            <ul><li>미수최대가능</li><li>주</li></ul>
+            <div >
+                <p>주문금액</p>
+                <button>예약매수</button>
+                <button>현금매수</button>
+            </div>
+        </div>
     );
 }
 
 const Sell = () : JSX.Element => {
     return (
-        <>
-            sell
-        </>
+        <div id='sell'>
+            <ul>
+                <li><NavLink to=''>지정가</NavLink></li>
+                <li><NavLink to=''>시장가</NavLink></li>
+            </ul>
+            <div><input type='number' />원</div>
+            <div><input type='number' />주</div>
+            <ul><li>매도가능</li><li>주</li></ul>
+            <div >
+                <p>주문금액</p>
+                <button>예약매도</button>
+                <button>현금매도</button>
+            </div>
+        </div>
     );
 }
 
 const Revise = () : JSX.Element => {
     return (
-        <>
+        <div id='revise'>
             revise
-        </>
+        </div>
     );
 }
 
 const OrderList = () : JSX.Element => {
     return (
-        <>
+        <div id='orderlist'>
             orderlist
-        </>
+        </div>
     );
 }
 
