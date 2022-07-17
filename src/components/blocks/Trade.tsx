8fdf4e6eca96b948ea59da3bd9897b1b43241b59
남현우 <hwnim5324@naver.com>
2022-07-17 23:56:20 +0900

    trade in progress

--- src/components/blocks/Trade.tsx
+++ src/components/blocks/Trade.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Link, NavLink } from "react-router-dom";
 
@@ -37,30 +37,36 @@ const Trading = ( props : { children : React.ReactNode } ) : JSX.Element => {
 
 const TradeNavigator = () : JSX.Element => {
     return (
-        <ul>
-            <li><NavLink to='/trading/buy' className={({ isActive }) => isActive ? 'link-active' : 'link'}>매수</NavLink></li>
-            <li><NavLink to='/trading/sell' className={({ isActive }) => isActive ? 'link-active' : 'link'}>매도</NavLink></li>
-            <li><NavLink to='/trading/revise' className={({ isActive }) => isActive ? 'link-active' : 'link'}>정정/취소</NavLink></li>
-            <li><NavLink to='/trading/orderlist' className={({ isActive }) => isActive ? 'link-active' : 'link'}>체결/예약</NavLink></li>
-        </ul>
+        <nav>
+            <ul>
+                <li><NavLink to='/trading/buy' className={({ isActive }) => isActive ? 'link-active' : 'link'}>매수</NavLink></li>
+                <li><NavLink to='/trading/sell' className={({ isActive }) => isActive ? 'link-active' : 'link'}>매도</NavLink></li>
+                <li><NavLink to='/trading/revise' className={({ isActive }) => isActive ? 'link-active' : 'link'}>정정/취소</NavLink></li>
+                <li><NavLink to='/trading/orderlist' className={({ isActive }) => isActive ? 'link-active' : 'link'}>체결/예약</NavLink></li>
+            </ul>
+        </nav>
     );
 }
 
 const Buy = () : JSX.Element => {
 
     const LoginStore = useContext(LogInStore);
+    const [orderType,setOrderType] = useState('market');
+    const [price,setPrice] = useState(0);
+    const [number,setNumber] = useState(0);
 
     return LoginStore.isLogin?(
         <div id='buy'>
             <ul>
-                <li><NavLink to=''>지정가</NavLink></li>
-                <li><NavLink to=''>시장가</NavLink></li>
+                <li><button id={orderType==='market'?'clicked':'unclicked'} onClick={()=>{setOrderType('market');}}>지정가</button></li>
+                <li><button id={orderType==='pending'?'clicked':'unclicked'} onClick={()=>{setOrderType('pending');}}>시장가</button></li>
+                {/* 링크가 아니라 state에 따른 내부 컴포넌트 변경으로 바꾸기. */}
             </ul>
-            <div><input type='number' />원</div>
-            <div><input type='number' />주</div>
-            <ul><li>현금최대가능</li><li>주</li></ul>
-            <ul><li>미수최대가능</li><li>주</li></ul>
-            <div >
+            <div id='number'><input type='number' onChange={(e)=>{setPrice(Number(e.target.value));}} />원</div>
+            <div id='number'><input type='number' onChange={(e)=>{setNumber(Number(e.target.value));}} />주</div>
+            <ul><li>현금최대가능<span>주</span></li></ul>
+            <ul><li>미수최대가능<span>주</span></li></ul>
+            <div>
                 <p>주문금액</p>
                 <button>예약매수</button>
                 <button>현금매수</button>
@@ -78,8 +84,9 @@ const Sell = () : JSX.Element => {
     return (
         <div id='sell'>
             <ul>
-                <li><NavLink to=''>지정가</NavLink></li>
-                <li><NavLink to=''>시장가</NavLink></li>
+                <li><button>지정가</button></li>
+                <li><button>시장가</button></li>
+                {/* 링크가 아니라 state에 따른 내부 컴포넌트 변경으로 바꾸기. */}
             </ul>
             <div><input type='number' />원</div>
             <div><input type='number' />주</div>
