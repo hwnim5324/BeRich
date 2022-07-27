1f808126e3db03d9514f3da4beec2b762bd49b23
남현우 <hwnim5324@naver.com>
2022-07-27 22:33:53 +0900

    keyword sync in progress.

--- src/store/SearchStore.ts
+++ src/store/SearchStore.ts
@@ -1,35 +1,36 @@
-import { action, observable } from "mobx";
+import { action, observable, autorun } from "mobx";
 import { createContext } from "react";
-
 
 class SearchStore{
     @observable
-    keyword = '';
+    keyword: string = '삼성전자';
 
     @observable
-    searching = true;
+    startDate: string = '';
+
+    @observable
+    endDate: string = '';
+    
+    constructor(){
+        this.startDate = this.getDate(50);
+        this.endDate = this.getDate(0);
+    }
 
     @action
     setKeyword( keyword : string ){
         this.keyword = keyword;
     }
 
-    @action
-    getKeyword(){
-        return this.keyword;
-    }
 
-    @action
-    setSearching( searching : boolean ){
-        this.searching = searching;
-    }
-
-    @action
-    getSearching(){
-        return this.searching;
-    }
+    getDate( num: number ){
+        const date = new Date();
+        date.setDate(date.getDate() - num);
     
-
+        const year = date.getFullYear();
+        const month = ("0" + (1 + date.getMonth())).slice(-2);
+        const day = ("0" + date.getDate()).slice(-2);
+        return year + month + day;
+    }
 }
 
 export default createContext(new SearchStore());
