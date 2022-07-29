59cc80f24e572f60dadbd40c7e820d9840cdf929
남현우 <hwnim5324@naver.com>
2022-07-29 23:01:29 +0900

    mypage ui done.

--- src/components/pages/Mypage.tsx
+++ src/components/pages/Mypage.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+
+import '../../styles/pages/Mypage.scss';
 
 const MyPage = () : JSX.Element => {
+
+    const [pw, setPw] = useState('');
+    const [check, setCheck] = useState('');
+
     return (
-        <>
-            mypage
-        </>
+        <section id="Mypage">
+            <div id='password-input'>
+                <label>비밀번호</label>
+                <input type='password' onChange={(e)=>{setPw(e.target.value);}}/>
+            </div>
+            <div id='password-input'>
+                <label>비밀번호 확인</label>
+                <input type='password' onChange={(e)=>{setCheck(e.target.value);}}/>
+            </div>
+            <button id="btn-change">비밀번호 변경</button>
+            <button id="btn-logout">로그아웃</button>
+            <button id="btn-quit">탈퇴</button>
+        </section>
     );
 }
 
