124487b4f021c84d07becf09c1692d7168215947
남현우 <hwnim5324@naver.com>
2022-08-04 22:58:47 +0900

    main page ui in progress.

--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -7,8 +7,11 @@ import '../../styles/pages/Main.scss';
 const Main = () : JSX.Element => {
 
     return (
-        <section>
-            <p>이 페이지는 메인 페이지입니다.</p>
+        <section id='main'>
+            <div id='banner'>배너</div>
+            <div id='rank'>랭크</div>
+            <div id='notice'>공지</div>
+            <div id='board'>게시판</div>
         </section>
     );
 }
