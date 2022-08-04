1cad2fddcf041d2e695cfab12b7c7ad485446e50
남현우 <hwnim5324@naver.com>
2022-08-04 17:51:48 +0900

    my page in progress.

--- src/components/pages/Mypage.tsx
+++ src/components/pages/Mypage.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import Axios from "../../hooks/Axios";
+
 import '../../styles/pages/Mypage.scss';
 
 const MyPage = () : JSX.Element => {
@@ -17,11 +19,36 @@ const MyPage = () : JSX.Element => {
                 <label>비밀번호 확인</label>
                 <input type='password' onChange={(e)=>{setCheck(e.target.value);}}/>
             </div>
-            <button id="btn-change">비밀번호 변경</button>
-            <button id="btn-logout">로그아웃</button>
-            <button id="btn-quit">탈퇴</button>
+            <button id="btn-change" onClick={()=>{checkPw(pw, check);}}>비밀번호 변경</button>
+            <button id="btn-logout" onClick={()=>{logOut();}}>로그아웃</button>
+            <button id="btn-quit" onClick={()=>{quit();}}>탈퇴</button>
         </section>
     );
+};
+
+
+function checkPw(pw: string, check: string){
+    const DIFF = '비밀번호와 확인용 비밀번호가 일치하지 않습니다.';
+    if(pw===check){
+        //통신.
+    }else{
+        alert(DIFF);
+    }
+}
+
+function logOut(){
+    sessionStorage.removeItem('userCode');
+    document.location.href='/';
+}
+
+function quit(){
+    let answer = window.confirm('탈퇴하시면 가지고있는 재산과 주식은 사라집니다.\n정말 탈퇴하시겠습니까?');
+    if(answer){
+        let userCode = sessionStorage.getItem('userCode');
+        //통신
+    }else{
+
+    }
 }
 
 export default MyPage;
