9b9c010e938f4bdce20089fe04a74e4ffa5927cc
남현우 <hwnim5324@naver.com>
2022-08-11 18:33:50 +0900

    main page layout done.

--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -9,9 +9,18 @@ const Main = () : JSX.Element => {
     return (
         <section id='main'>
             <div id='banner'>배너</div>
-            <div id='rank'>랭크</div>
-            <div id='notice'>공지</div>
-            <div id='board'>게시판</div>
+            <article>
+                <div id='left-side'>
+                    <div id='rank'>랭킹</div>
+                </div>
+                <div id='center'>
+                    <div>내용</div>
+                </div>
+                <div id='right-side'>
+                <div id='notice'>공지</div>
+                <div id='board'>게시판</div>
+                </div>
+            </article>
         </section>
     );
 }
